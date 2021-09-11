56634fae66eecb6c7231e2bc3095480c9e1d4037
Siroyan <1517053@ed.tus.ac.jp>
2021-09-11 05:41:42 +0000

    First commit

---
+++ src/main/java/net/siroyan/dev/mc/plugin/entrynotification/App.java
@@ -0,0 +1,14 @@
+package net.siroyan.dev.mc.plugin.entrynotification;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
---
+++ src/test/java/net/siroyan/dev/mc/plugin/entrynotification/AppTest.java
@@ -0,0 +1,20 @@
+package net.siroyan.dev.mc.plugin.entrynotification;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
